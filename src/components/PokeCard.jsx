790bca9c6f59d0a5e3601b7b051cb91d9ff7a3d6
Jackie <jacquelinederigs@gmail.com>
2025-03-29 11:42:53 +0100

    updates

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const PokeCard = ({ pokemon, pokemonId, roster, setRoster }) => {
 	const isInRoster = roster.some((p) => p.id === pokemonId); // Check if the Pokémon is already in the roster
+	const location = useLocation(); // Get the current route
+	const isHomePage = location.pathname === "/"; // Check if the current route is the home page
+	const isMyRosterPage = location.pathname === "/my-roster"; // Check if the current route is the my-roster page
 
 	useEffect(() => {
 		// Update localStorage whenever the roster changes
 		localStorage.setItem("roster", JSON.stringify(roster));
 	}, [roster]); // Runs every time the roster changes
+
+	const removePokemon = (pokemonId) => {
+		const updatedRoster = roster.filter((pokemon) => pokemon.id !== pokemonId);
+		setRoster(updatedRoster);
+	};
+
+	const addPokemon = (pokemonId, pokemonName) => {
+		setRoster([...roster, { id: pokemonId, name: pokemonName }]);
+	};
 
 	return (
 		<div className="card bg-base-100 w-96 shadow-sm">
@@ -23,17 +36,21 @@ const PokeCard = ({ pokemon, pokemonId, roster, setRoster }) => {
 					<Link to={`/pokemon/${pokemonId}`} className="btn btn-primary">
 						See Entry
 					</Link>
-					<button
-						className="btn btn-primary"
-						onClick={() => {
-							if (!isInRoster) {
-								setRoster([...roster, { id: pokemonId, name: pokemon.name }]); // Add the Pokémon to the roster
-							}
-						}}
-					>
-						{" "}
-						{isInRoster ? "Caught ✅" : "Catch"}
-					</button>
+					{isHomePage ? (
+						<button
+							className="btn btn-primary"
+							onClick={() => addPokemon(pokemonId, pokemon.name)}
+						>
+							{isInRoster ? "Caught ✅" : "Catch"}
+						</button>
+					) : isMyRosterPage ? (
+						<button
+							className="btn btn-primary"
+							onClick={() => removePokemon(pokemonId)}
+						>
+							Remove
+						</button>
+					) : null}
 				</div>
 			</div>
 		</div>
