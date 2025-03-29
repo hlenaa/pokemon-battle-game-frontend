fd1f8d3cc2d3b14fb243f6c28cba90ca32d8e44b
Lena Hauer <lena.hauer@me.com>
2025-03-29 16:45:23 +0100

    Added Roster LocalStorage to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
@@ -13,43 +13,47 @@ const basepath = "/";
 export const AppContext = createContext();
 
 function App() {
-	const [pokemons, setPokemons] = useState([]); //initital state is empty array
-	const [roster, setRoster] = useState(() => {
-		return JSON.parse(localStorage.getItem("roster")) || [];
-	}); //initital state is empty array or localStortage
-	const [status, setStatus] = useState("loading"); //initital state for fetching data -> for UX
-	const [userScore, setUserScore] = useState(0); //initital state for user score
-	const [leaderboard, setLeaderboard] = useState([]);
+  const [pokemons, setPokemons] = useState([]); //initital state is empty array
+  const [roster, setRoster] = useState(() => {
+    return JSON.parse(localStorage.getItem("roster")) || [];
+  }); //initital state is empty array or localStortage
+  const [status, setStatus] = useState("loading"); //initital state for fetching data -> for UX
+  const [userScore, setUserScore] = useState(0); //initital state for user score
+  const [leaderboard, setLeaderboard] = useState([]);
 
-	return (
-		<AppContext.Provider
-			value={{
-				pokemons,
-				setPokemons,
-				status,
-				setStatus,
-				roster,
-				setRoster,
-				leaderboard,
-				setLeaderboard,
-				userScore,
-				setUserScore,
-			}}
-		>
-			<BrowserRouter>
-				<Routes>
-					<Route path={basepath} element={<MainLayout />}>
-						<Route index element={<Home />} />
-						<Route path="pokemon/:id" element={<PokemonDetail />} />
-						<Route path="my-roster" element={<PersonalRoster />} />
-						<Route path="battle" element={<Battle />} />
-						<Route path="leaderboard" element={<Leaderboard />} />
-						<Route path="NotFound" element={<NotFound />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</AppContext.Provider>
-	);
+  useEffect(() => {
+    localStorage.setItem("roster", JSON.stringify(roster));
+  }, [roster]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        pokemons,
+        setPokemons,
+        status,
+        setStatus,
+        roster,
+        setRoster,
+        leaderboard,
+        setLeaderboard,
+        userScore,
+        setUserScore,
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path={basepath} element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="pokemon/:id" element={<PokemonDetail />} />
+            <Route path="my-roster" element={<PersonalRoster />} />
+            <Route path="battle" element={<Battle />} />
+            <Route path="leaderboard" element={<Leaderboard />} />
+            <Route path="NotFound" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
